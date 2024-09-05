ca48dc92471d4a4994096b45f25f67bc47802917
SeoungwooHan <150769291+SeungwooHH11@users.noreply.github.com>
2024-09-05 15:44:43 +0900

    Add files via upload

---
+++ MultiHeadAttention_basic.py
@@ -0,0 +1,291 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+import torch.optim as optim
+import numpy as np
+device='cuda'
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model, num_heads):
+        super(MultiHeadAttention, self).__init__()
+        assert d_model % num_heads == 0
+
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.d_k = d_model // num_heads
+
+        self.q_linear = nn.Linear(d_model, d_model)
+        self.k_linear = nn.Linear(d_model, d_model)
+        self.v_linear = nn.Linear(d_model, d_model)
+        self.out = nn.Linear(d_model, d_model)
+
+    def attention(self, q, k, v, mask=None):
+        scores = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(self.d_k)
+        # q(batch_size, num_heads, seq_len, d_k)
+        # k.transpose(-2, -1) (batch_size, num_heads, d_k, seq_len)
+        # scores (batch_size, num_heads, seq_len, seq_len)
+        # mask (batch_size, num_heads, seq_len, seq_len)
+        
+        
+        if mask is not None:
+            mask_expanded = mask.unsqueeze(1)
+            scores = scores.masked_fill(mask_expanded == 0, -1e9)
+        scores = F.softmax(scores, dim=-1)
+        tensor_max = torch.max(scores, dim=-1, keepdim=True).values
+        scores = scores - tensor_max
+        output = torch.matmul(scores, v)
+        return output
+
+    def forward(self, q, k, v, mask=None):
+        batch_size = q.size(0)
+
+        # Linear projection and split into num_heads
+        q = self.q_linear(q).view(batch_size, -1, self.num_heads, self.d_k).transpose(1, 2)
+        # (batch_size, num_heads, seq_len, d_k)
+        k = self.k_linear(k).view(batch_size, -1, self.num_heads, self.d_k).transpose(1, 2)
+        v = self.v_linear(v).view(batch_size, -1, self.num_heads, self.d_k).transpose(1, 2)
+
+        # Apply attention on all the projected vectors
+        attn_output = self.attention(q, k, v, mask)
+        # attn_output: batch_size, num_heads, seq_len, d_k
+        # Concatenate heads and put through final linear layer
+        attn_output = attn_output.transpose(1, 2).contiguous().view(batch_size, -1, self.d_model)
+        output = self.out(attn_output)
+        return output
+
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model, dim_feedforward):
+        super(FeedForward, self).__init__()
+        self.linear1 = nn.Linear(d_model, dim_feedforward)
+        self.linear2 = nn.Linear(dim_feedforward, d_model)
+
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
+        x = self.linear2(x)
+        return x
+
+
+class TransformerEncoderLayer(nn.Module):
+    def __init__(self, d_model, num_heads, dim_feedforward, dropout=0.1):
+        super(TransformerEncoderLayer, self).__init__()
+        self.self_attn = MultiHeadAttention(d_model, num_heads)
+        self.feed_forward = FeedForward(d_model, dim_feedforward)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+    def forward(self, src, src_mask=None):
+        src2 = self.self_attn(src, src, src, src_mask)
+        src = src + self.dropout1(src2)
+        src = self.norm1(src)  # layer norm
+        src2 = self.feed_forward(src)
+        src = src + self.dropout2(src2)
+        src = self.norm2(src)  # layer norm
+        return src
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(self, encoder_layer, num_layers):
+        super(TransformerEncoder, self).__init__()
+        self.layers = nn.ModuleList([encoder_layer for _ in range(num_layers)])
+        self.num_layers = num_layers
+
+    def forward(self, src, mask=None):
+        output = src
+        for layer in self.layers:
+            output = layer(output, mask)
+        return output
+
+
+class Transformer(nn.Module):
+    def __init__(self, input_fea_len, d_model, num_heads, num_layers, dim_feedforward):
+        super(Transformer, self).__init__()
+        self.embedding = nn.Linear(input_fea_len, d_model)
+        encoder_layer = TransformerEncoderLayer(d_model, num_heads, dim_feedforward)
+        self.encoder = TransformerEncoder(encoder_layer, num_layers)
+        
+
+    def forward(self, src, src_mask=None):
+        src = self.embedding(src)
+        output = self.encoder(src, src_mask)
+        return output
+
+class MLP(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(MLP, self).__init__()
+
+        self.fc1 = nn.Linear(input_size, 64)
+        self.fc2 = nn.Linear(64, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, output_size)
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                # He 초기화를 사용하여 가중치를 초기화합니다.
+                nn.init.kaiming_uniform_(m.weight, mode='fan_in', nonlinearity='relu')
+                # 편향을 0으로 초기화합니다.
+                nn.init.zeros_(m.bias)
+            elif isinstance(m, nn.BatchNorm1d):
+                # 배치 정규화 레이어의 가중치를 초기화합니다.
+                nn.init.ones_(m.weight)
+                nn.init.zeros_(m.bias)
+                
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return x
+
+class PPO(nn.Module):
+    def __init__(self, learning_rate=0.001, clipping_ratio=0.2, job_len=130, machine_len=12, d_model=512, num_heads=16, fea_len=6,num_layers=3,
+                 dim_feedforward=1024):
+        super(PPO, self).__init__()
+        
+        self.encoder = Transformer(fea_len, d_model, num_heads, num_layers, dim_feedforward)
+        #self.decoder = Transformer(machine_fea_len, d_model, num_heads, num_layers, dim_feedforward)
+        self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+        self.gamma = 0.99
+        self.lmbda = 0.95
+        self.epsilon=clipping_ratio
+        self.alpha = 0.5
+        
+        self.AA=MLP(d_model,1)
+        self.CA=MLP(d_model,1)
+        self.job_len=job_len
+        self.machine_len=machine_len
+        
+    def create_masking(self,mask):
+        # mask의 크기는 (batch, seq)
+        batch_size, seq_len = mask.size()
+
+        # 1차원 마스크를 (seq, 1)로 확장하여 서로 곱할 수 있게 만듭니다.
+        mask = mask.unsqueeze(2)  # 크기: (batch, seq, 1)
+
+        # 마스크를 transpose하고 서로 곱해서 (seq, seq) 크기로 확장합니다.
+        masking = mask * mask.transpose(1, 2)  # 크기: (batch, seq, seq)
+
+        return masking
+
+    def get_action(self, state, mask_seq, ans=None):
+        # mask_seq [1,0,....,1] (batch, seq) 1이 고려함
+        # state (batch, n+m, fea)
+        batch,seq,fea=state.shape
+        machine_mask = torch.ones((batch, self.machine_len)).to(device)  # 예: (3, 2) 크기의 텐서
+        total_mask = torch.cat((mask_seq.clone(), machine_mask), dim=1)
+        mask = self.create_masking(total_mask)
+        enh = self.encoder(state, mask) #(batch,n+m,fea)
+        tensor = enh[:, :self.job_len, :]
+        #mask masking=0
+        
+        output=self.AA(tensor) #(batch,n,1)
+        
+        if mask is not None:
+            mask_expanded = mask_seq.unsqueeze(2)
+            output = output.masked_fill(mask_expanded == 0, -1e9)
+        
+        tensor_max = torch.max(output, dim=1, keepdim=True).values
+        output = output- tensor_max
+        output = F.softmax(output, dim=1)
+        output = output.squeeze(-1)
+        
+        samples = torch.multinomial(output, 1)  # (B, 1) 크기의 인덱스 텐서
+        if ans!=None:
+            pi = output.squeeze(-1).gather(1, ans)
+        else:
+            pi = output.squeeze(-1).gather(1, samples)
+        
+        return samples, pi
+
+    def calculate_v(self, state, mask_seq):
+        # mask_seq [1,0,....,1] (batch, seq) 1이 고려함
+        
+        batch,seq,fea=state.shape
+        
+        machine_mask = torch.ones((batch, self.machine_len)).to(device)  # 예: (3, 2) 크기의 텐서
+        total_mask = torch.cat((mask_seq.clone(), machine_mask), dim=1)
+        
+        mask = self.create_masking(total_mask)
+        enh = self.encoder(state, mask) #(batch,n+m,fea)
+        fea=enh.shape[2]
+        if mask is not None:
+            mask_expanded = total_mask.unsqueeze(2).repeat(1,1,fea)
+            
+            enh ==enh.masked_fill(mask_expanded == 0, 0) 
+        enh=torch.sum(enh, dim=1, keepdim=True) 
+        state_value=self.CA(enh).squeeze(-1) #(batch,1,fea)
+        
+        return state_value
+
+    def update(self, episode, k_epoch,step1,model_dir):
+
+        states = [item[0] for item in episode]
+        states = torch.stack(states, dim=0).squeeze(1).to(device)
+        
+        batch, seq, fea = states.shape
+        actions = [item[1] for item in episode]
+        actions = torch.tensor(actions).unsqueeze(1).to(device)
+        pi_old = [item[2] for item in episode]
+        pi_old = torch.tensor(pi_old).unsqueeze(1).to(device)
+        rewards = [item[3] for item in episode]
+        rewards = torch.tensor(rewards).unsqueeze(1).to(device)
+        dones = [item[4] for item in episode]
+        dones = torch.tensor(dones).unsqueeze(1).to(device)
+        masks = [item[5] for item in episode]  # (1,seq)
+        masks = torch.stack(masks, dim=0).squeeze(1).to(device)
+        
+        for _ in range(k_epoch):
+            
+            _,pi_new = self.get_action(states, masks, actions)
+            state_v = self.calculate_v(states, masks)
+            
+            state_next_v = state_v[1:].clone()
+            zero_row = torch.zeros(1, 1).to(device)
+
+            state_next_v = torch.cat((state_next_v, zero_row), dim=0)
+            
+            td_target = rewards + self.gamma * state_next_v * dones
+            delta = td_target - state_v
+            advantage_lst = np.zeros(len(episode))
+            advantage_lst = torch.tensor(advantage_lst, dtype=torch.float32).unsqueeze(1).to(device)
+            episode_num = int(len(episode) //self.job_len)
+            j = 0
+                
+            for i in range(episode_num):
+                advantage = 0.0
+                for t in reversed(range(j, j + self.job_len)):
+                    
+                    advantage = self.gamma * self.lmbda * advantage + delta[t][0]
+                    advantage_lst[t][0] = advantage
+                    
+                j += self.job_len
+                
+            
+            ratio = torch.exp(torch.log(pi_new) - torch.log(pi_old))  # a/b == exp(log(a)-log(b))
+            surr1 = ratio * advantage_lst
+            surr2 = torch.clamp(ratio, 1 - self.epsilon, 1 + self.epsilon) * advantage_lst
+            loss = -torch.min(surr1, surr2) + F.smooth_l1_loss(state_v, td_target.detach()) * self.alpha
+            
+            ave_loss = loss.mean().item()
+            v_loss = (self.alpha * F.smooth_l1_loss(state_v, td_target.detach())).item()
+            
+            p_loss = -torch.min(surr1, surr2).mean().item()
+
+            self.optimizer.zero_grad()
+            loss.mean().backward()
+            self.optimizer.step()
+        
+        if step1 % 100 == 0:
+            torch.save({
+                'model_state_dict': self.state_dict(),
+                'optimizer_state_dict': self.optimizer.state_dict(),
+
+            }, model_dir+'trained_model' + str(step1) + '.pth')
+
+        return ave_loss, v_loss, p_loss
+
---
+++ Simulator_PMSP_basic.py
@@ -0,0 +1,204 @@
+import random
+import pandas as pd
+import heapq
+import numpy as np
+from MultiHeadAttention_basic import *
+random.seed(42)
+device='cuda'
+import torch
+class PMSPScheduler:
+    def __init__(self, num_machines=12, initial_jobs=80, additional_jobs=10, additional_arrivals=5, 
+                 processing_time_range=(5, 15), machine_speed=[1,1,1,1,1,1,1.5,1.5,1.5,1.5,1.5,1.5], start_additional_arrival=20, arrival_interval=10,setup_range=(5,15),family_setup_num=10):
+        self.num_machines = num_machines
+        self.initial_jobs = initial_jobs
+        self.additional_jobs = additional_jobs
+        self.additional_arrivals = additional_arrivals
+        self.processing_time_range = processing_time_range
+        self.start_additional_arrival = start_additional_arrival
+        self.arrival_interval = arrival_interval
+        self.MP=float(10*(initial_jobs+additional_jobs*additional_arrivals)/num_machines+10*(8+130)/2/12)
+        self.tardiness_factor=0.1
+        self.duedate_range=0.5
+        self.schedule = []
+        self.machine_speed=machine_speed
+        self.setup_range=setup_range
+        self.setup_num=family_setup_num
+        A = np.random.uniform(setup_range[0], setup_range[1], (family_setup_num, family_setup_num))
+        symmetric_A = np.triu(A) + np.triu(A, 1).T
+        np.fill_diagonal(symmetric_A, 0)
+        self.setup=symmetric_A
+        
+    def generate_jobs(self):
+        jobs = np.empty((0, 8))
+        job_count=0
+        for i in range(self.initial_jobs):
+            job_id = job_count
+            arrival_time = 0
+            processing_time = random.randint(*self.processing_time_range)
+            tardy_time=np.random.uniform(max(arrival_time+processing_time+10,self.MP*(1-self.tardiness_factor-self.duedate_range/2)),self.MP*(1-self.tardiness_factor+self.duedate_range/2))
+            setup_type=np.random.randint(0, self.setup_num)
+            property_j=[job_id,arrival_time,processing_time,setup_type,tardy_time,0.0,1.0, 0.0]
+            jobs = np.vstack([jobs, np.array(property_j)])
+            job_count+=1
+
+        for j in range(self.additional_arrivals):
+            arrival_time = self.start_additional_arrival + j * self.arrival_interval
+            for k in range(self.additional_jobs):
+                job_id = job_count
+                processing_time = random.randint(*self.processing_time_range)
+                tardy_time=np.random.uniform(max(arrival_time+processing_time+10,self.MP*(1-self.tardiness_factor-self.duedate_range/2)),self.MP*(1-self.tardiness_factor+self.duedate_range/2))
+                setup_type=np.random.randint(0, self.setup_num)
+                property_j=[job_id,arrival_time,processing_time,setup_type,tardy_time,0.0,0.0, 0.0]
+                jobs = np.vstack([jobs, np.array(property_j)])
+                job_count+=1
+        return jobs
+    
+    def get_agent(self, machine_end_time):
+        # 리스트에서 최소값을 찾습니다.
+        min_value = min(machine_end_time)
+        # 최소값의 모든 인덱스를 찾습니다.
+        min_indices = [i for i, x in enumerate(machine_end_time) if x == min_value]
+        # 최소값의 인덱스 중 랜덤하게 하나를 선택합니다.
+        chosen_index = random.choice(min_indices)
+        return min_value, chosen_index
+    def calculate_tardy(self, job_u_s,current_time):
+        
+        # job_id / arrival_time / processing_time / familiy_type / tardy_time / tardy_occur /mask / processed 여부
+        tardy_occured = job_u_s[:,-3].sum()
+        not_processed=job_u_s[job_u_s[:,-1]==0].copy()
+        tardy_yet=np.maximum(not_processed[:,2]+current_time-not_processed[:,-3],0).sum()
+        return tardy_occured+tardy_yet
+ 
+    
+    def schedule_jobs(self,jobs,episode,ppo):
+        machines = []
+        jobss = []
+        start_times = []  # start times as floats
+        durations = []      # durations as floats
+        total_tardy=0
+        total_reward=0
+        
+        past_tardy=0
+        # 각 머신의 작업 큐를 저장할 리스트 (우선순위 큐로 구현)
+        machine_matrix=np.zeros((self.num_machines,3))
+        machine_matrix[:,2]=self.machine_speed
+        # remainning processing time, current setup, speed
+        current_time=0.0
+        # job_id / arrival_time / processing_time / familiy_type / tardy_time / tardy_occur /mask / processed 여부
+        job_len=jobs.shape[0]
+        # job_id / arrival_time / processing_time / familiy_type / tardy_time / tardy_occur /mask / processed 여부
+        jobs_u_s=jobs.copy()
+        state=np.zeros((job_len+self.num_machines,6))
+        
+        for i in range(job_len):
+            min_value, chosen_index=self.get_agent(machine_matrix[:,0].copy()) # batch, n+m, fea
+            current_time+=min_value
+            machine_matrix[:,0]=machine_matrix[:,0]-min_value
+            # 현재 시간보다 작고 processing이 되지 않은 job의 마스킹 변경, 
+            condition = (jobs_u_s[:, -1] == 0) & (jobs_u_s[:, 1] <=current_time)
+            # 조건을 만족하는 행의 1번째 열 값을 1로 변경
+            jobs_u_s[condition, -2] = 1
+            
+            count = np.sum(jobs_u_s[:, -2] == 1)
+            while count==0:
+                filtered_arr = jobs_u_s[jobs_u_s[:, -1] == 0]
+                min_row = np.min(filtered_arr[:, 1])
+                gap=min_row-current_time
+                current_time=min_row
+                machine_matrix[:,0]=machine_matrix[:,0]-gap
+                machine_matrix[machine_matrix[:, 0] < 0, 0] = 0
+                
+                condition = (jobs_u_s[:, -1] == 0) & (jobs_u_s[:, 1] <=current_time)
+                # 조건을 만족하는 행의 1번째 열 값을 1로 변경
+                jobs_u_s[condition, -2] = 1
+                count = np.sum(jobs_u_s[:, -2] == 1)
+
+            mask=torch.tensor(jobs_u_s[:,-2].copy(),dtype=torch.float32).unsqueeze(0).to(device) #(1, seq)
+            # job_id / arrival_time / processing_time / familiy_type / tardy_time / tardy_occur /mask / processed 여부
+            job_state=jobs_u_s[:,2:-3].copy() #processing / family type / tardy
+            job_state[:,-1]=job_state[:,-1]-current_time
+            state[:job_len,:3]=job_state
+            machine_pro = machine_matrix[chosen_index,:]
+            
+            # (6,3) 크기로 행을 복제
+            machine_pro = np.tile(machine_pro, (job_len, 1))
+            state[:job_len,3:]=machine_pro
+            state[job_len:,3:]=machine_matrix
+
+            
+            state_tensor=torch.tensor(state.copy(),dtype=torch.float32).unsqueeze(0).to(device) # batch, n+m, fea
+            
+            action,pi=ppo.get_action(state_tensor,mask,ans=None)
+            job_index=action.item()
+            # job_id / arrival_time / processing_time / familiy_type / tardy_time / tardy_occur /mask / processed 여부
+            machine_matrix[chosen_index,0]=jobs_u_s[job_index][2]/machine_matrix[chosen_index,2]+self.setup[int(machine_matrix[chosen_index,1])][int(jobs_u_s[job_index][3])]
+            machine_matrix[chosen_index,1]=jobs_u_s[job_index][3]
+            jobs_u_s[job_index,-2]=0
+            jobs_u_s[job_index,-1]=1
+
+            total_tardy+=max((current_time+machine_matrix[chosen_index,0])-jobs_u_s[job_index][-4],0)
+            # job_id / arrival_time / processing_time / familiy_type / tardy_time / tardy_occur /mask / processed 여부
+            jobs_u_s[job_index][-3]=max((current_time+machine_matrix[chosen_index,0])-jobs_u_s[job_index][-4],0)
+            tardy=self.calculate_tardy(jobs_u_s,current_time)
+            reward=past_tardy-tardy
+            total_reward+=reward
+            past_tardy=tardy
+            
+           
+            #episode.append([job_state.clone(), machine_state.clone(), action, pi, reward, dones, mask, job_next_state.clone(), machine_next_state.clone()])
+            if i==jobs.shape[0]-1:
+                done=0
+            else:
+                done=1
+            
+            episode.append([state_tensor.clone(), action.item(), pi.item(), reward, done, mask.clone()])
+            # job_id / arrival_time / processing_time / familiy_type / tardy_time / tardy_occur /mask / processed 여부
+            machines.append("Machine "+str(chosen_index))
+            jobss.append("Job "+str(job_index))
+            start_times.append(current_time)
+            durations.append(jobs_u_s[job_index][2])
+            
+        return machines,jobss,start_times,durations,episode,total_tardy,total_reward
+     
+    def plot_gantt(self,machines,jobs,start_times,durations):
+                # Prepare data for Gantt chart
+        df = pd.DataFrame({
+            'Machine': machines,
+            'Job': jobs,
+            'Start': start_times,
+            'Finish': [start_times[i] + durations[i] for i in range(len(start_times))]
+        })
+        
+        # Create Gantt chart
+        fig, ax = plt.subplots(figsize=(10, 6))
+
+        for i, row in df.iterrows():
+            ax.barh(row['Machine'], row['Finish'] - row['Start'], left=row['Start'], label=row['Job'])
+
+        # Add labels and title
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Machine')
+        ax.set_title('Gantt Chart')
+
+        # Optional: Add job labels inside the bars
+        for i, row in df.iterrows():
+            ax.text(row['Start'] + (row['Finish'] - row['Start']) / 2, row['Machine'], row['Job'],
+                    va='center', ha='center', color='white', fontweight='bold')
+
+        plt.show()
+    
+    def run_simulation(self,cat_num,cat_num2,ppo):
+        episode=[]
+        ave_tardy=[]
+        for __ in range(cat_num):
+            jobs=self.generate_jobs()
+            jobs[:,1:]=jobs[:,1:].astype(float)
+            for _ in range(cat_num2):  
+                machines,jobss,start_times,durations,episode,total_tardy,total_reward=self.schedule_jobs(jobs,episode,ppo)
+                ave_tardy.append(total_tardy)
+            #self.plot_gantt(machines,jobss,start_times,durations)
+        ave_t=np.array(ave_tardy).mean()
+        return ave_t,ave_tardy,episode
+# 결과를 저장할 경우
+    
+# schedule_df.to_csv("schedule_results.csv", index=False)

bf402e72121171a50456bcd8d1c7da41a9b6520c
SeoungwooHan <150769291+SeungwooHH11@users.noreply.github.com>
2024-11-21 17:17:54 +0900

    Update train_PMSP.py

--- Fs/train_PMSP.py
+++ Fs/train_PMSP.py
@@ -31,7 +31,7 @@ if __name__=="__main__":
     number_of_validation=10
     number_of_validation_batch=100
     number_of_problem=2# 한번에 몇개의 문제를
-    number_of_batch=8 # 문제당 몇 episode씩 한번에 학습할껀지
+    number_of_batch=6 # 문제당 몇 episode씩 한번에 학습할껀지
     number_of_trial=1  #1, 10, 100, 1000 #이를 몇번 반복할껀지
     number_of_iteration=int(1501/number_of_trial)  # 전체 iteration #iteration 단위로 문제 변화
     validation=[]

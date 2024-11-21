249692df8557669db35adcc739be37c2309eafed
SeoungwooHan <150769291+SeungwooHH11@users.noreply.github.com>
2024-11-21 17:17:22 +0900

    Update train_PMSP.py

--- Fs/train_PMSP.py
+++ Fs/train_PMSP.py
@@ -26,19 +26,19 @@ if __name__=="__main__":
     #PMSP=PMSPScheduler(10,100,20,10,(10,20),[1,1,1,1,1,1.5,1.5,1.5,1.5,1.5],1,60,60,(5,15),10)
     #PMSP=PMSPScheduler(10,0,20,15,(10,20),[1,1,1,1,1,1.5,1.5,1.5,1.5,1.5],1,0,60,(5,15),10)
     PMSP=PMSPScheduler(10,140,20,8,(10,20),[1,1,1,1,1,1.5,1.5,1.5,1.5,1.5],1,60,60,(5,15),10)
-    ppo=PPO(learning_rate=0.001, clipping_ratio=0.2, machine_len=10, d_model=512, num_heads=8, fea_len=25,num_layers=1,dim_feedforward=1024).to(device)
+    ppo=PPO(learning_rate=0.001, clipping_ratio=0.2, machine_len=10, d_model=256, num_heads=8, fea_len=25,num_layers=1,dim_feedforward=512).to(device)
 
     number_of_validation=10
     number_of_validation_batch=100
     number_of_problem=2# 한번에 몇개의 문제를
-    number_of_batch=4 # 문제당 몇 episode씩 한번에 학습할껀지
+    number_of_batch=8 # 문제당 몇 episode씩 한번에 학습할껀지
     number_of_trial=1  #1, 10, 100, 1000 #이를 몇번 반복할껀지
-    number_of_iteration=int(2/number_of_trial)  # 전체 iteration #iteration 단위로 문제 변화
+    number_of_iteration=int(1501/number_of_trial)  # 전체 iteration #iteration 단위로 문제 변화
     validation=[]
     validation_step = 100
     num_of_meta=6
     history = np.zeros((number_of_iteration * number_of_trial,2))
-    validation_history=np.zeros((int(2/validation_step)+10,number_of_validation))
+    validation_history=np.zeros((int(1501/validation_step)+10,number_of_validation))
     control=np.zeros((num_of_meta,number_of_validation))
     validation_job=[]
     validation_setup=[]

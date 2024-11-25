56fa81ee8e8c49703f9b31fd1324c3727947831a
SeoungwooHan <150769291+SeungwooHH11@users.noreply.github.com>
2024-11-26 01:15:47 +0900

    Update train_PMSP.py

--- Fs/train_PMSP.py
+++ Fs/train_PMSP.py
@@ -26,7 +26,7 @@ if __name__=="__main__":
     #PMSP=PMSPScheduler(10,100,20,10,(10,20),[1,1,1,1,1,1.5,1.5,1.5,1.5,1.5],1,60,60,(5,15),10)
     #PMSP=PMSPScheduler(10,0,20,15,(10,20),[1,1,1,1,1,1.5,1.5,1.5,1.5,1.5],1,0,40,(5,15),10)
     #PMSP=PMSPScheduler(10,300,20,0,(10,20),[1,1,1,1,1,1.5,1.5,1.5,1.5,1.5],1,60,60,(5,15),10)
-    PMSP=PMSPScheduler(num_machines=6, initial_jobs=6, additional_jobs=6, additional_arrivals=15, processing_time_range=(10, 20), machine_speed=[1,1,1,1.5,1.5,1.5], sim_type=1, start_additional_arrival=20, arrival_interval=20,setup_range=(5,15),family_setup_num=4)
+    PMSP=PMSPScheduler(num_machines=6, initial_jobs=3, additional_jobs=3, additional_arrivals=31, processing_time_range=(10, 20), machine_speed=[1,1,1,1.5,1.5,1.5], sim_type=1, start_additional_arrival=20, arrival_interval=10,setup_range=(5,15),family_setup_num=4)
     #PMSP=PMSPScheduler(num_machines=16, initial_jobs=400, additional_jobs=15, additional_arrivals=8, processing_time_range=(5, 15), machine_speed=[1,1,1,1,1,1,1,1,1.25,1.25,1.25,1.25,1.25,1.25,1.25,1.25], sim_type=0, start_additional_arrival=20, arrival_interval=10,setup_range=(5,15),family_setup_num=8)
     ppo=PPO(learning_rate=0.001, clipping_ratio=0.2, machine_len=6, d_model=256, num_heads=4, fea_len=13,num_layers=1,dim_feedforward=512).to(device)
 
